cdf6f621ed4be9155f959a59c45de01eaf435567
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-03-25 09:32:44 -0400

    first commit

---
+++ api/recipe-json.js
@@ -0,0 +1,19 @@
+
+const recipes = {
+    'ginger beer': {
+        id: 1,
+        name: '5 Ingedient Ginger Beer Margaritas',
+        description: '5-Ingredient margaritas made with ginger beer and fresh lime juice! Simple, flavorful, and so delicious. The only margarita recipe you’ll ever need.',
+        author: 'Minimalist Baker',
+        ingredients: { // Ingredient: []
+            'Agave Tequila': ['1 1/2 oz', '3 Tbsp', 'such as Altos'],
+            'Lime Juice': ['1 oz', '2 Tbsp'],
+            'Ginger Beer': ['3 oz', '6 Tbsp'],
+            'Simple Syrup': ['1/2 oz', '1 Tbsp'],
+            'Ice': ['1/2 cup', 'plus more for serving'],
+            'Coarse salt': ['for lining rim', 'optional']
+        }
+    }
+}
+
+module.exports = recipes;
---
+++ api/server.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const recipes = require('./recipe-json')
+
+const PORT = process.env.PORT || 8000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'), () => console.log(`Server responding...`))
+})
+
+app.get('/api/recipes', (req, res) => {
+    res.json(recipes)
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
